8b40510b31f3f4d8ab738c805fea30eb6f6cd6e0
Walter Cuberas <waltercuberas@MacBook-Air-de-Waty.local>
2021-02-26 18:16:13 -0300

    Ultima homework 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -135,7 +135,7 @@ function operadoresLogicos(num1, num2, num3) {
     return 'Hay negativos';
   }else if (num1 === 0 || num2 === 0 || num3 === 0){
     return 'Error';
-  } else if (num1 > num2 && num1 > num3 && num1 > 0){
+  } else if (num1 > num2 > num3) {//else if (num1 > num2 && num1 > num3 && num1 > 0){
     return 'Número 1 es mayor y positivo';
   }else if (num3 > num1 && num3 > num2){
     num3 ++;
